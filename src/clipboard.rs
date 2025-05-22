2cfe88152e5ed65d39d9a87ee9ce975c7e2f1d97
lowerc4s3 <kkirill03@gmail.com>
2025-05-22 10:13:43 +0300

    refactor(clipboard): general improvments
    - decompose errors
    - shorten function names
    - move clipboard helper function into utils module

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -3,7 +3,6 @@ use std::fs::{self, File};
 use std::io::{self, BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
 
-use fs4::fs_std::FileExt;
 use thiserror::Error;
 
 #[derive(Debug)]
@@ -16,20 +15,19 @@ impl Clipboard {
         Clipboard { path }
     }
 
-    pub fn add_files<T: AsRef<Path>>(&mut self, files: &[T]) -> Result<(), AddFilesError> {
-        let mut clipboard_file = File::options()
+    pub fn add<T: AsRef<Path>>(&mut self, files: &[T]) -> Result<(), AddError> {
+        let mut clip_file = File::options()
             .write(true)
             .read(true)
             .create(true)
             .truncate(false)
             .open(&self.path)
-            .map_err(AddFilesError::Open)?;
-
-        lock_file(&mut clipboard_file)?;
+            .map_err(FileError::Access)?;
+        utils::lock_file(&mut clip_file)?;
 
         let mut buf = String::new();
-        clipboard_file.read_to_string(&mut buf).map_err(AddFilesError::IO)?;
-        let clipboard_contents: HashSet<_> = buf.lines().map(Path::new).collect();
+        clip_file.read_to_string(&mut buf).map_err(FileError::Access)?;
+        let contents: HashSet<_> = buf.lines().map(Path::new).collect();
 
         let files = files
             .iter()
@@ -37,32 +35,32 @@ impl Clipboard {
             .map(|filename| {
                 filename
                     .canonicalize()
-                    .map_err(|e| AddFilesError::Add { filename: filename.to_path_buf(), source: e })
+                    .map_err(|e| AddError::AbsPath { filename: filename.to_path_buf(), source: e })
             })
             .collect::<Result<HashSet<_>, _>>()?;
 
         // TODO: Return error if there're no new files to add
-        let mut clipboard_writer = BufWriter::new(clipboard_file);
-        files
-            .iter()
-            .filter(|filename| !clipboard_contents.contains(filename.as_path()))
-            .try_for_each(|path| -> Result<(), _> {
-                writeln!(clipboard_writer, "{}", path.display()).map_err(AddFilesError::IO)
-            })
+        let mut clip_writer = BufWriter::new(clip_file);
+        files.iter().filter(|filename| !contents.contains(filename.as_path())).try_for_each(
+            |path| -> Result<(), _> {
+                writeln!(clip_writer, "{}", path.display()).map_err(FileError::Access)
+            },
+        )?;
+        Ok(())
     }
 
-    pub fn copy_files(&mut self, dest: Option<&Path>) -> Result<(), AddFilesError> {
+    pub fn copy_to(&mut self, dest: Option<&Path>) -> Result<(), CopyError> {
         todo!();
     }
 
-    pub fn move_files(&mut self, dest: Option<&Path>) -> Result<(), MoveFilesError> {
+    pub fn move_to(&mut self, dest: Option<&Path>) -> Result<(), MoveError> {
         // TODO: Handle symlinks
-        let dest = dest.unwrap_or(Path::new(".")).canonicalize()?;
-        let selected = self.get_selected()?;
+        let dest = dest.unwrap_or(Path::new(".")).canonicalize().map_err(|_| DestDirError)?;
+        let selected = self.contents()?;
         let mut failed: Vec<PathBuf> = Vec::with_capacity(selected.len());
 
         for filename in selected {
-            if let Err(e) = move_file(&filename, &dest) {
+            if let Err(e) = utils::move_file(&filename, &dest) {
                 eprintln!("Failed to move {}: {e}", filename.display());
                 failed.push(filename);
             }
@@ -76,77 +74,52 @@ impl Clipboard {
         Ok(())
     }
 
-    pub fn get_selected(&self) -> Result<Vec<PathBuf>, FileError> {
-        Ok(fs::read_to_string(&self.path)?.lines().map(PathBuf::from).collect())
+    pub fn contents(&self) -> Result<Vec<PathBuf>, FileError> {
+        let mut file = File::open(&self.path)?;
+        utils::lock_file(&mut file)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        Ok(contents.lines().map(PathBuf::from).collect())
     }
 
     pub fn clear(&mut self) -> Result<(), FileError> {
-        File::options().write(true).truncate(true).open(&self.path)?;
+        let mut clip_file = File::options().write(true).open(&self.path)?;
+        utils::lock_file(&mut clip_file)?;
+        clip_file.set_len(0)?;
         Ok(())
     }
 
     fn add_overwrite<T: AsRef<Path>>(&mut self, files_abs: &[T]) -> Result<(), FileError> {
-        let mut clipboard_file =
-            BufWriter::new(File::options().write(true).truncate(true).open(&self.path)?);
+        let mut clip_file = File::options().write(true).open(&self.path)?;
+        utils::lock_file(&mut clip_file)?;
+        clip_file.set_len(0)?;
+        let mut clip_writer = BufWriter::new(clip_file);
         for filepath in files_abs {
-            writeln!(clipboard_file, "{}", filepath.as_ref().display())?;
+            writeln!(clip_writer, "{}", filepath.as_ref().display())?;
         }
         Ok(())
     }
 }
 
-fn move_file(filename: &Path, dest: &Path) -> Result<(), MoveFilesError> {
-    let new_filename = dest.join(
-        filename.file_name().ok_or_else(|| MoveFilesError::Basename(filename.to_path_buf()))?,
-    );
-    fs::rename(filename, &new_filename).map_err(|e| MoveFilesError::Move {
-        from: filename.to_path_buf(),
-        to: new_filename,
-        source: e,
-    })?;
-    Ok(())
-}
-
-fn lock_file(file: &mut File) -> Result<(), LockError> {
-    match file.try_lock_exclusive() {
-        Err(e) => Err(LockError{ source: Some(e) }),
-        Ok(false) => Err(LockError{ source: None }),
-        _ => Ok(()),
-    }
-}
-
 #[derive(Debug, Error)]
-#[error("cannot access clipboard file")]
-pub struct FileError(#[from] io::Error);
-
-#[derive(Debug, Error)]
-#[error("other process uses clipboard file")]
-pub struct LockError {
-    source: Option<io::Error>,
-}
-
-#[derive(Debug, Error)]
-pub enum AddFilesError {
-    #[error("cannot open clipboard file")]
-    Open(io::Error),
-
+pub enum AddError {
     #[error("cannot process clipboard file")]
-    IO(io::Error),
+    File(#[from] FileError),
 
-    #[error("cannot add file {} to clipboard file", .filename.display())]
-    Add { filename: PathBuf, source: io::Error },
+    #[error("cannot get absolute path of {}", .filename.display())]
+    AbsPath { filename: PathBuf, source: io::Error },
 
     #[error(transparent)]
-    Lock(#[from] LockError),
+    Lock(#[from] utils::LockError),
 }
 
 #[derive(Debug, Error)]
-pub enum CopyFilesError {}
+pub enum CopyError {}
 
 #[derive(Debug, Error)]
-pub enum MoveFilesError {
-    #[error("cannot resolve destination dir")]
-    GetCWD(#[from] io::Error),
+pub enum MoveError {
+    #[error(transparent)]
+    DestinationDir(#[from] DestDirError),
 
     #[error(transparent)]
     Read(#[from] FileError),
@@ -156,4 +129,47 @@ pub enum MoveFilesError {
 
     #[error("cannot move {} to {}", .from.display(), .to.display())]
     Move { from: PathBuf, to: PathBuf, source: io::Error },
+}
+
+#[derive(Debug, Error)]
+pub enum FileError {
+    #[error("cannot access clipboard file")]
+    Access(#[from] io::Error),
+
+    #[error(transparent)]
+    Lock(#[from] utils::LockError),
+}
+
+#[derive(Debug, Error)]
+#[error("cannot resolve destination dir")]
+pub struct DestDirError;
+
+mod utils {
+    use super::*;
+    use fs4::fs_std::FileExt;
+
+    pub(super) fn move_file(filename: &Path, dest: &Path) -> Result<(), MoveError> {
+        let new_filename = dest
+            .join(filename.file_name().ok_or_else(|| MoveError::Basename(filename.to_path_buf()))?);
+        fs::rename(filename, &new_filename).map_err(|e| MoveError::Move {
+            from: filename.to_path_buf(),
+            to: new_filename,
+            source: e,
+        })?;
+        Ok(())
+    }
+
+    pub(super) fn lock_file(file: &mut File) -> Result<(), LockError> {
+        match file.try_lock_exclusive() {
+            Err(e) => Err(LockError { source: Some(e) }),
+            Ok(false) => Err(LockError { source: None }),
+            _ => Ok(()),
+        }
+    }
+
+    #[derive(Debug, Error)]
+    #[error("other process uses clipboard file")]
+    pub struct LockError {
+        source: Option<io::Error>,
+    }
 }
